2f401c66973fb807fc2e49b3d0b4954292248d3e
dav.i <davilfranca06@gmail.com>
2023-08-01 10:18:05 -0300

    first commit

---
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css'],
+})
+export class PensamentoComponent implements OnInit {
+  constructor() {}
+
+  @Input() pensamento: Pensamento = {
+    id: 0,
+    conteudo: 'I love Angular',
+    autoria: 'Davi',
+    modelo: 'modelo3',
+  };
+
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g';
+    } else {
+      return 'pensamento-p';
+    }
+  }
+
+  ngOnInit(): void {}
+}
